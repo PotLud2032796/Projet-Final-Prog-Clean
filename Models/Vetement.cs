e17917f2573e88357e843a70a3236ba093b0fb63
Édouard Hamel <1833263@etu.cegepjonquiere.ca>
2023-04-18 16:25:34 -0400

    second commit

---
+++ Models/Vetement.cs
@@ -0,0 +1,13 @@
+﻿namespace Atelier.Models
+{
+    public class Vetement
+    {
+        public int VetementId { get; set; }
+        public string Nom { get; set; }
+        public string? ProprietaireId { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime DateObtention { get; set; }
+        public string Image { get; set; }
+    }
+}
